9ea58348ddff5b3f372be14600e325408872a844
Tayanithaa N S <Tayanithaans2196@gmail.com>
2026-07-05 23:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.39%) - LeetSync

---
+++ codes/1289-day-of-the-week/day-of-the-week.py
@@ -0,0 +1,10 @@
+import datetime
+class Solution(object):
+    def dayOfTheWeek(self, day, month, year):
+        """
+        :type day: int
+        :type month: int
+        :type year: int
+        :rtype: str
+        """
+        return datetime.date(year,month,day).strftime("%A")
