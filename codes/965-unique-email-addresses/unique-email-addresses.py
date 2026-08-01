8d85f35c915f38f3ab27de895833f406f2a3231d
Tayanithaa N S <Tayanithaans2196@gmail.com>
2026-08-02 00:23:03 +0530

    Time: 5 ms (82.08%) | Memory: 12.5 MB (1.63%) - LeetSync

---
+++ codes/965-unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        unique=set()
+        for i in emails:
+            local,domain=i.split("@")
+            local=local.split("+")[0]
+            local=local.replace(".","")
+            unique.add(local+"@"+domain)
+        return len(unique)
