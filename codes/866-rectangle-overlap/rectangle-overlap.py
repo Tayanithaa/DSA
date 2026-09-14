4732cf8a368e449e42dbb0e3f7d9189745eea2a9
Tayanithaa N S <Tayanithaans2196@gmail.com>
2026-09-14 11:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.89%) - LeetSync

--- codes/866-rectangle-overlap/rectangle-overlap.py
+++ codes/866-rectangle-overlap/rectangle-overlap.py
@@ -5,5 +5,16 @@ class Solution(object):
         :type rec2: List[int]
         :rtype: bool
         """
-        return (min(rec1[2], rec2[2]) > max(rec1[0], rec2[0])) and \
-               (min(rec1[3], rec2[3]) > max(rec1[1], rec2[1]))
+        x1,y1,x2,y2= rec1[0],rec1[1],rec1[2],rec1[3]
+        x3,y3,x4,y4 = rec2[0],rec2[1],rec2[2],rec2[3]
+
+        if x2<=x3:
+            return False
+        if x1>=x4:
+            return False
+        if y2<=y3:
+            return False
+        if y1>=y4:
+            return False
+
+        return True
